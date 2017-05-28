35ba5b2598e6774e0b3e05d734a23857964e52c5
Jonny Ervine <jervine@users.noreply.github.com>
2017-05-28 14:59:08 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+##
+## Start up script for sonarr on CentOS docker container
+##
+
+## Initialise any variables being called:
+# Set the correct timezone
+TZ=${TZ:-UTC}
+USER=$USER
+USERUID=$USERUID
+setup=/config/.setup
+
+if [ ! -f "${setup}" ]; then
+  rm -f /etc/localtime
+  cp /usr/share/zoneinfo/$TZ /etc/localtime
+  adduser -u $USERUID $USER
+  sed -i "s/user=/user=$USER/g" /etc/supervisord.d/sonarr.ini
+  touch $setup
+fi
+
+## Start up sonarr daemon via supervisord
+/usr/bin/supervisord -n -c /etc/supervisord.conf
